e1f4ef69690aa3c17171a814e8c6c9532fce2073
Hugo <hugo.benedicto.plagnes@gmail.com>
2025-02-04 19:31:43 +0100

    coucou

--- test_graphe.cpp
+++ test_graphe.cpp
@@ -6,7 +6,7 @@ int main() {
         {1, 2, 15}, {1, 5, 4}, {3, 2, 3}, {3, 4, 2}, {4, 1, 10}, {4, 2, 3}, {5, 3, 7}, {5, 4, 5}
     };
     cout << "Nombre de sommets : " << g.nombreSommets << endl;
-    //cout << "Arcs : " << g.listeArcs << endl; pas de méthode implémentée pour afficher liste d'arcs pour l'instant
+    //cout << "Arcs : " << g.listeArcs << endl; pas de méthode implémentée pour afficher liste d'arcs pour l'instantgit
     cout << "Voisins de 4 : " << g.voisins(4) << endl;
     cout << "Successeurs de 4 : " << g.successeurs(4) << endl;
     cout << "Prédécesseurs de 4 : " << g.predecesseurs(4) << endl;
