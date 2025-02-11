2b5af57673ae0507437c585ae3f6abfe7e0942f4
Hugo <hugo.benedicto.plagnes@gmail.com>
2025-02-11 12:10:37 +0100

    Modifié test_dijksta

--- test_dijkstra.cpp
+++ test_dijkstra.cpp
@@ -9,18 +9,23 @@ int main() {
         {4, 2, 3}, {5, 3, 7}, {5, 4, 5}
     };
 
+    Graph g1 = {
+        {1, 5, 0.7}, {5, 1 , 0.7}, {1, 4, 1.58}, {4, 1, 1.58}, {1, 2, 1.58},
+        {2, 1, 1.58}, {5, 4, 1}, {4, 5, 1}, {4, 3, 1}, {3, 4, 1}, {3, 2, 1}, {2, 3, 1}, {5, 2, 1}, {2, 5, 1}, {4, 6, 0.7}, {6, 4, 0.7}, {3, 6 ,0.7}, {6, 3, 0.7}
+    };
+
     // Exécuter Dijkstra à partir du sommet 1
-    auto [l, p] = dijkstra(g, 1);
+    auto [l, p] = dijkstra(g1, 1);
 
     // Affichage des distances minimales
     std::cout << "Distances minimales (l) :\n";
-    for (size_t i = 1; i <= g.nombreSommets; i++) {
+    for (size_t i = 1; i <= g1.nombreSommets; i++) {
         std::cout << "l[" << i << "] = " << l[i] << "\n";
     }
 
     // Affichage des prédécesseurs
     std::cout << "\nPrédécesseurs (p) :\n";
-    for (size_t i = 1; i <= g.nombreSommets; i++) {
+    for (size_t i = 1; i <= g1.nombreSommets; i++) {
         std::cout << "p[" << i << "] = " << p[i] << "\n";
     }
 
