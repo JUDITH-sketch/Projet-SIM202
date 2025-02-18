449e17eb92dabd3c187d438303fbce6db4ab3377
Alice Olejniczak <aliceolejniczak@MacBook-Air-de-Alice.local>
2025-02-18 11:48:44 +0100

    ajout de l'exemple 4

--- exemple4.cpp
+++ exemple4.cpp
@@ -13,8 +13,8 @@ int main() {
     Sommet B(1.5, 1.5);
 
     // ========== Définition de l'Obstacle ==========
-    Sommet S1(-0.5, 0), S2(0.5, 0), S3(0.5, 3), S4(-0.5, 3);
-    Obstacle O1({S1, S2, S3, S4});
+    Sommet S1(-0.5, 2.5), S2(-0.5, -1), S3(0.5, 1), S4(0.5, 2.5), S5(0.32,2.71), S6(0.1, 2.8), S7(-0.14, 2.8), S8(-0.28, 2.7), S9(-0.42, 2.54);
+    Obstacle O1({S1, S2, S3, S4, S5, S6, S7, S8, S9});
     Gobstacle G01(O1, 1);
 
     // ========== Construction du Graphe ==========
