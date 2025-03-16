21503651410efb11d450dd6694f6895d46978014
Hugo <hugo.benedicto.plagnes@gmail.com>
2025-03-16 16:03:36 +0100

    ajout de deux test dans intersection

--- test_intersection.cpp
+++ test_intersection.cpp
@@ -8,12 +8,17 @@ int main() {
     // Définition des points de base
     Sommet e1(1, 0), e2(0, 1);
 
-    // Sommets de l'obstacle (ordre important)
+    // Sommets du carré (ordre important)
     Sommet S1(0, 0), S2(0, 2), S3(2, 2), S4(2, 0);
+
+    // Sommets du L 
+    Sommet S5(-1.5, 2), S6(-1, 2), S7(-1, 0.32), S8(0.99, 0.3), S9(0.99, -0.1), S10(-1.51, -0.09);
+    
 
     // Création de l'obstacle (carré de longueur 2)
     Obstacle O({S1, S2, S3, S4});
-    std::cout << "Obstacle créé :\n";
+    Obstacle O1({S5, S6, S7, S8, S9, S10});
+    std::cout << "Obstacles créé :\n";
 
     // Test d'intersection avec un segment qui traverse l'obstacle
     Segment S(e1 - e2, e1 + 3.0 * e2); // Segment (1,-1) -> (1,3)
@@ -44,4 +49,14 @@ int main() {
     Segment E(S1, e1 + e2); 
     std::cout << "\nSegment testé : " << E << "\n";
     std::cout << "Intersection avec l'obstacle ? " << (O.intersection(E, methode) ? "TRUE" : "FALSE") << "\n"; // Doit afficher TRUE
+
+    // Test d'intersection avec un segment contenu entièrement dans l'obstacle
+    Segment F(0.5 * e1 + e2, 1.5 * e1 + e2);
+    std::cout << "\nSegment testé : " << F << "\n";
+    std::cout << "Intersection avec l'obstacle ? " << (O.intersection(F, methode) ? "TRUE" : "FALSE") << "\n"; // Doit afficher TRUE
+    
+    // Test d'intersection avec le L 
+    Segment G(S6, S8);
+    std::cout << "\nSegment testé : " << G << "\n";
+    std::cout << "Intersection avec l'obstacle ? " << (O1.intersection(F, methode) ? "TRUE" : "FALSE") << "\n"; // Doit afficher FALSE
 }
